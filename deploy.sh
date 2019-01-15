ae47230a7b4d36611d47bf461f35fffc88c4ae93
Sreedharan <sreeramachandran-stacklighting>
2019-01-15 11:08:57 +0530

    Deployment Process

---
+++ deploy.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+current_git_branch() {
+     git rev-parse --abbrev-ref HEAD
+}
+
+hubj_deployment(){
+	echo "*** Deployment On Hubj Started ***"
+}
+
+tagging_process(){
+	echo "*** Tagging Process Started ***"
+
+}
+
+
+
+
+# determine the version automatically from the current branch name and past tags
+
+
+auto_assign_version() {
+	echo "*** Tagging Process Started ***"
+	CURRENT_BRANCH=$(current_git_branch)
+	EXT_VERSION=${BASH_REMATCH[1]}
+
+
+}
+
+
+
+CURRENT_BRANCH=$(current_git_branch)
+
+if echo "$CURRENT_BRANCH" | grep 'release'; then
+	echo "*** Currently You Are In Release Branch ***";
+else
+	echo "*** Currently You Are In Other Branch Checkout To Release Branch And Contine With Process***";
+	exit 1
+fi
+
+echo "***Your Current Git Branch Is ***" $CURRENT_BRANCH;
+
+
+read -p "Continue Your Process With Deployment Or Tagging (D/T)?" CONTINUE
+
+if [ "$CONTINUE" = "D" ]; then
+ 	hubj_deployment
+else
+    auto_assign_version
+fi
+
+
+
+
+
+#CURRENT_BRANCH= git rev-parse --abbrev-ref HEAD
