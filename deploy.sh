8c4d5be26bf181f4d74838dd7e1824e40081eb9f
Sreedharan <sreeramachandran-stacklighting>
2019-01-16 16:05:28 +0530

    Commit Check

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ tagging_process() {
   		echo "***Working Directory Is clean Continuing With Process****"
 
 		LAST_TAG_VERSION=$(last_tag_version)
-
+		
 		#replace . with space so can split into an array
 		VERSION_BITS=(${LAST_TAG_VERSION//./ })
 
@@ -62,12 +62,18 @@ tagging_process() {
 		read -p "Continue Your Process With Creating Tag (Y/N)?" CONTINUE
 
 		if [ "$CONTINUE" = "Y" ]; then
- 			echo "*** Creating New Tag And Pushing To Origin ***"
- 			    # create new local tag
-    		git tag "${NEW_TAG_VERSION}"
+			#get current hash and see if it already has a tag
+			GIT_COMMIT=`git rev-parse HEAD`
+			NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
-    		# create new remote tag
-    		git push origin "${NEW_TAG_VERSION}"
+			#only tag if no tag already (would be better if the git describe command above could have a silent option)
+		if [ -z "$NEEDS_TAG" ]; then
+    		echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+    		git tag $NEW_TAG
+    		git push --tags
+		else
+    		echo "Already a tag on this commit"
+		fi
 
 		else
     		echo "*** Tagging Process Aborted ***"
