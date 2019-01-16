1378607f14612b0cf2be48075c30e4870961ec2f
Sreedharan <sreeramachandran-stacklighting>
2019-01-16 17:51:43 +0530

    Commit Check Updated

--- deploy.sh
+++ deploy.sh
@@ -69,8 +69,8 @@ tagging_process() {
 
 			#only tag if no tag already (would be better if the git describe command above could have a silent option)
 			if [ -z "$NEEDS_TAG" ]; then
-    			echo "Tagged with $NEW_TAG_VERSION (Ignoring fatal:cannot describe - this means commit is untaggeds) "
-    			git tag $NEW_TAG_VERSION
+    			echo "Tagged with $NEW_TAG_VERSION (Ignoring fatal:cannot describe - this means commit is untagged) "
+    			git tag -a $NEW_TAG_VERSION
     			git push --tags
 			else
     			echo "Already a tag on this commit"
