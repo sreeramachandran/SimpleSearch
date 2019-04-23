fcb5c52b3e42a137fd54b67937ff5c2987f1da34
Sreedharan <sreeramachandran-stacklighting>
2019-04-23 11:35:10 +0530

    Tagging

--- deploy.sh
+++ deploy.sh
@@ -51,7 +51,7 @@ pull_current_git_branch(){
 check_previous_tag_version_on_origin(){
 	LAST_TAG_VERSION=$(last_tag_version)
 	echo "Last Tag Is >>> " $LAST_TAG_VERSION
-	VNUM4=git ls-remote --tags origin | grep "$LAST_TAG_VERSION"
+	VNUM4=$(git ls-remote --tags origin | grep "$LAST_TAG_VERSION")
 	echo $VNUM4
 
 
